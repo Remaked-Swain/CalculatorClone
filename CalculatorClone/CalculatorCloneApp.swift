8c6193e908d4eb4aab449f60a8aedab372ab2a18
YunSwain <destap@naver.com>
2023-08-05 17:59:54 +0900

    Creation

---
+++ CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by Swain Yun on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
